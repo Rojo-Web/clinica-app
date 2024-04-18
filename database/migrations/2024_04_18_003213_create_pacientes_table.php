7488a25489522fb4c32821d05d13a059356c37fd
Rojo-Web <santifa2004@gmail.com>
2024-04-17 19:50:16 -0500

    Migraciones

---
+++ database/migrations/2024_04_18_003213_create_pacientes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',255)->nullable();
+            $table->string('apellido',255)->nullable();
+            $table->date('fecha_nacimiento')->nullable();
+            $table->char('genero',2)->nullable();
+            $table->text('direccion')->nullable();
+            $table->string('telefono',255)->nullable();
+            $table->string('email',255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pacientes');
+    }
+};

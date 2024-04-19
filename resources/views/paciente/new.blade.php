d6f16974f9ec4aa04e437320e7fcafb2e030a0a6
JoanWolf <dasstian@hotmail.com>
2024-04-18 23:50:43 -0500

    ajustes vista create pacientes

--- resources/views/paciente/new.blade.php
+++ resources/views/paciente/new.blade.php
@@ -22,70 +22,96 @@
         </div>
         <form method="POST" action="{{ route('pacientes.store')}}">
             @csrf
-            <div class="mb-3">
-                <label for="id" class="form-label">Codigo</label>
-                <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id" disabled="disabled">
-                <div id="idHelp" class="form-text">Codigo del paciente</div>
+            <div class="row">
+
+
+                <div class="col-md-4">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Nombre">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="mb-3">
+                        <label for="apellido" class="form-label">Apellido</label>
+                        <input type="text" class="form-control" id="apellido" name="apellido" placeholder="Apellido">
+                    </div>
+                </div>
+
             </div>
+
 
 
 
             <!-- ============================================================================= -->
 
-            <div class="container mt-3">
-    <div class="row align-items-center">
-        <div class="col-md-4">
-            <div class="mb-3">
-                <label for="name" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Nombre">
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="mb-3">
-                <label for="apellido" class="form-label">Apellido</label>
-                <input type="text" class="form-control" id="apellido" name="apellido" placeholder="Apellido">
-            </div>
-        </div>
-    </div>
+            <div class="container mt-2">
+                <div class="row align-items-center">
 
-    <div class="row align-items-center">
-        <div class="col-md-4">
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="mb-3">
-                <label for="fecha_nacimiento" class="form-label">Fecha de Nacimiento</label>
-                <input type="date" class="form-control" id="fecha_nacimiento" name="fecha_nacimiento">
-            </div>
-        </div>
-    </div>
 
-    <div class="row align-items-center">
-        <div class="col-md-4">
-            <div class="mb-3">
-                <label for="direccion" class="form-label">Dirección</label>
-                <input type="text" class="form-control" id="direccion" name="direccion" placeholder="Dirección">
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="mb-3">
-                <label for="telefono" class="form-label">Teléfono</label>
-                <input type="text" class="form-control" id="telefono" name="telefono" placeholder="Teléfono">
-            </div>
-        </div>
-    </div>
+                    <div class="col-md-4">
+                        <div class="mb-3">
+                            <label for="id" class="form-label">Codigo</label>
+                            <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id" disabled="disabled" placeholder="Codigo">
+                            <!-- <div id="idHelp" class="form-text">Codigo del paciente</div> -->
+                        </div>
+                    </div>
 
-    <!-- ============================BOTONES=============================================== -->
-    <div class="row align-items-center mt-3">
-        <div class="col">
-            <button type="submit" class="btn btn-danger">Save</button>
-            <a href="{{ route('pacientes.index') }}" class="btn btn-warning ms-2">Cancel</a>
-        </div>
-    </div>
-</div>
+
+                    <div class="col-md-4">
+                        <div class="mb-3">
+                            <label for="genero" class="form-label">Genero  </label>
+                            <div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" value="" id="generoM" name="generoM">
+                                    <label class="form-check-label" for="generoM">M</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" value="" id="generoF" name="generoF">
+                                    <label class="form-check-label" for="generoF">F</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row align-items-center">
+                        <div class="col-md-4">
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="mb-3">
+                                <label for="fecha_nacimiento" class="form-label">Fecha de Nacimiento</label>
+                                <input type="date" class="form-control" id="fecha_nacimiento" name="fecha_nacimiento">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row align-items-center">
+                        <div class="col-md-4">
+                            <div class="mb-3">
+                                <label for="direccion" class="form-label">Dirección</label>
+                                <input type="text" class="form-control" id="direccion" name="direccion" placeholder="Dirección">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="mb-3">
+                                <label for="telefono" class="form-label">Teléfono</label>
+                                <input type="text" class="form-control" id="telefono" name="telefono" placeholder="Teléfono">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- ============================BOTONES=============================================== -->
+                    <div class="row align-items-center mt-3">
+                        <div class="col">
+                            <button type="submit" class="btn btn-danger">Save</button>
+                            <a href="{{ route('pacientes.index') }}" class="btn btn-warning ms-2">Cancel</a>
+                        </div>
+                    </div>
+                </div>
 
 
 
